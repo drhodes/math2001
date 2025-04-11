6b22a2ba4773fe6ca05bc00e227298bb6ca4a38a
Derek Rhodes <physci@gmail.com>
2025-04-11 14:56:57 -0400

    disable autograder for now

--- Math2001/Homework/hw0.lean
+++ Math2001/Homework/hw0.lean
@@ -1,7 +1,7 @@
 /- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
 import Mathlib.Tactic.GCongr
 import Library.Basic
-import AutograderLib
+-- import AutograderLib
 
 math2001_init
 
@@ -11,6 +11,6 @@ Don't forget to compare with the text version
 for clearer statements and any special instructions. -/
 
 
-@[autograded 5]
+-- @[autograded 5]
 theorem problem1 {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
   sorry
