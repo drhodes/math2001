4c8ae6f377c255d348274de12034b8a89a50916a
Derek Rhodes <physci@gmail.com>
2025-04-19 15:16:44 -0400

    building with mathlib at cd4edcb7d79

--- lakefile.lean
+++ lakefile.lean
@@ -27,35 +27,7 @@ want also
 but currently only Lean core options can be set in lakefile
 -/
 
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"9d6b50d77c4"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"565cdcb266f"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"e23b08714da"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"b6d00f332a4"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"9a07518812f"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"bd6616c746d"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"132e5112a46"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"7458f0e73e0"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"132e5112a46"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"d3a6c9f3bd1"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"feec58a7ee9"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"v4.5.0"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"e1d0b5c3f8c"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"14151c79a2c"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"e7d0ec3b0e6"
--- ok require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"86ecbac93d1"
-
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"490d2d4820d"
-
--- broken require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"007ad7420b9"
--- broken require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"7509e796a5f"
--- broken require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"7af47e01b4d"
--- broken require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"3a748a56b49"
--- broken require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"a55d6e57970"
--- broken require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"6bd7a4911dc"
--- broken require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"3007dc8384a"
--- broken require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"b1b4ebf76b3"
--- broken require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"3752900e2f1"
--- broken require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"490d2d4820d"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"cd4edcb7d79"
 
 
 --require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"v{Lean.versionString}"
