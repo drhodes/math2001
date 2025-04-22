a8ab578a40e1591b235c32b455e16490e7e80fd2
Derek Rhodes <physci@gmail.com>
2025-04-21 23:31:51 -0400

    removes deprecated import

--- Library/Theory/Comparison.lean
+++ Library/Theory/Comparison.lean
@@ -1,7 +1,6 @@
 /- Copyright (c) Mario Carneiro, 2023. -/
 import Mathlib.Data.Int.Order.Basic
-import Mathlib.Init.Data.Nat.Lemmas
+--import Mathlib.Init.Data.Nat.Lemmas
 
 macro "le_or_succ_le" a:term:arg n:num  : term =>
   `(show $a ≤ $n ∨ $(Lean.quote (n.getNat+1)) ≤ $a from le_or_lt ..)
-
