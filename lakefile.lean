c259fad5e5806439b11b002499fdce69f4d99e18
Derek Rhodes <physci@gmail.com>
2025-04-22 15:25:39 -0400

    updates to mathlib@v4.16.0, toolchain@v4.16.0

--- lakefile.lean
+++ lakefile.lean
@@ -27,7 +27,7 @@ want also
 but currently only Lean core options can be set in lakefile
 -/
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"v4.15.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"v4.16.0"
 
 
 --require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"v{Lean.versionString}"
